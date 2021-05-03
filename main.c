a4138170406a7d20b15ccf1b24f7167009e21d8e
Bori <toluwabori,akinola@ucdconnect.ie>
2021-05-03 19:10:26 +0100

    game didn;t skip turn when no valid move was possible

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 #include "board.h"
 #include "player.h"
 #include "gamePlay.h"
@@ -16,10 +17,13 @@ int main(void){
     printf("%s goes first\n", p1.name);
 
 
-    while(numOfTiles != SIZE*SIZE && (checkMoves(p1.color) || checkMoves(p2.color))){
-
-        playPlayer(p1);
-        playPlayer(p2);
+    while(numOfTiles != SIZE*SIZE){
+        if(checkMoves(p1.color))
+            playPlayer(p1);
+        else if(checkMoves(p2.color))
+            playPlayer(p2);
+        else
+            break;
     }
 
     printf("Game Over!\n");
@@ -30,5 +34,15 @@ int main(void){
         printf("%s wins!!!\n", p1.name);
     }
 
+    FILE *fp;
+    time_t t;
+    time(&t);
+
+    fp = fopen("/Users/boriakinola/Desktop/CompProgProject1/Othello/othello-results.txt", "a");
+
+    fprintf(fp,"\nGame played on %s\n", ctime(&t));
+    fprintf(fp, "Player 1: %s \tScore: %d\n"
+                "Player 2: %s \tScore: %d\n", p1.name,
+                p1.score, p2.name, p2.score);
     return 0;
 }
